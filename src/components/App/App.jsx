c50c826fe8dc10533b6f2f928fac6c6f52527d2f
Johnshandsable <Jshands@asu.edu>
2021-02-19 16:31:03 -0600

    finished client side get route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import axios from 'axios';
 
@@ -7,13 +7,32 @@ import Header from '../Header/Header';
 
 function App() {
 
+  // *********** ON-START ***************
+  useEffect(() => {
+    getData();
+  }, []);
+
+  // *********** VARIABLES ***************
+  const [galleryList, setGalleryList] = useState([]);
+
 
   // *********** ROUTES ***************
   function getData() {
     /*
-      GET ROUTE
+      GET ROUTE to /gallery
     */
-    console.log('CLIENT - GET ');
+    
+    axios.get('/gallery', (req, res) => {
+      console.log('CLIENT - GET inside getData() ');
+    })
+    .then((response) => {
+      console.log(' .then CLIENT - GET - got a response', response);
+      console.log(' .then CLIENT - GET - response.data', response.data);
+      setGalleryList(response.data)
+    })
+    .catch((error) => {
+      console.log('.catch CLIENT - GET - An error was received', error);
+    })
   }
 
     return (
