ae0953d7cbc58d82fa294adc752b8da3f686d08b
Johnshandsable <Jshands@asu.edu>
2021-02-19 16:14:48 -0600

    added Header component along with styling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+
+// *********** IMPORT COMPONENTS ***************
+import Header from '../Header/Header';
 
 function App() {
+
+
+  // *********** ROUTES ***************
+  function getData() {
+    /*
+      GET ROUTE
+    */
+    console.log('CLIENT - GET ');
+  }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
+      <div className="App"> 
+        <Header />
         <p>Gallery goes here</p>
         <img src="images/goat_small.jpg"/>
       </div>
